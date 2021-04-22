6fee634f60a542d179c3350939c0a62c1f75dfd7
Max <max.mjm.melo@gmail.com>
2021-04-22 08:57:37 -0400

    pasta e preparação de método do exercício 1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # VC_trab2
-Segundo trabalho de visão computacional
+
+Segundo trabalho de visão computacional.
+
+## Organização
+
+### Exercício 1 - Convolução
+
+### Exercício 2 - Segmentação com K-Means
+
+### Exercício 3 - Subtração de fundo
+
+## Referências
---
+++ ex1-convolucao/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def convolution(img, kernel):
+    '''
+    It runs a convolution in the img based on a given kernel
+    img a PIL image instance
+    kernel an array with odd dimensions
+    returns an convoluted image or None if no convolution can be applied
+    '''
+    
+    pass
