5c09e88e66a15454d636bcd0169a0b528c4a9b57
Max <max.mjm.melo@gmail.com>
2021-04-24 21:15:43 -0400

    ex 1-a.

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ Segundo trabalho de visão computacional.
 
 ### Exercício 1 - Convolução
 
+- 1.a: Filtro de média aplicado por kernel 3x3 e 5x5. A imagem alvo é mostrada abaixo.
+![Imagem Original](/ex1-convolucao/imgs/noisy.jpg)
+
+  - O resultado da convolução com kernel 3x3 é mostrado abaixo.
+  ![Imagem Original](/ex1-convolucao/out/a_3_3.png)
+
+  - O resultado da convolução com kernel 5x5 é mostrado abaixo.
+  ![Imagem Original](/ex1-convolucao/out/a_5_5.png)
+
+As imagens de resultado possuem a mesma dimensão que a imagem de entrada visto padding=1 para o kernel 3x3 e padding=2 para o kernel 5x5.
+
 ### Exercício 2 - Segmentação com K-Means
 
 ### Exercício 3 - Subtração de fundo
--- ex1-convolucao/main.py
+++ ex1-convolucao/main.py
@@ -5,17 +5,18 @@ import cv2
 import os
 
 
-def convolution(img, kernel):
+def convolution(img, kernel, s=1, p=1):
     '''
     It runs a convolution in the img based on a given kernel
     img a PIL image instance
     kernel an array with odd dimensions
+    img: input image (expected as np array)
+    kernel: kernel to be applied in the convolution
+    s: convolution stride
+    p: convolution padding. Zero fill is applied.
     returns an convoluted image or None if no convolution can be applied
     '''
-
-    p = 1
-    s = 1
-
+    
     kwidth = kernel.shape[0]
     kheight = kernel.shape[1]
     ks = kwidth // 2
@@ -40,18 +41,10 @@ def convolution(img, kernel):
     else:
         imagePadded = img
 
-    for y in range(height):
-        if y > height-kheight:
-            break
-        if y % s == 0:
-            for x in range(width):
-                if x > width - kwidth:
-                    break
-                try:
-                    if x % s == 0:
-                        output[x, y] = (kernel*imagePadded[x:x+kwidth, y: y+kheight]).sum()
-                except:
-                    break
+    for y in range(0, height_out, s):
+        for x in range(0, width_out, s):
+            output[x, y] = (kernel*imagePadded[x:x+kwidth, y: y+kheight]).sum()
+                
     return output
 
 img    = Image.open('imgs/noisy.jpg')
@@ -59,7 +52,7 @@ img    = np.array(img)
 #kernel = np.array([[-1,-2,-1], [0,0,0], [1,2,1]], dtype='uint8')
 kernel = np.ones((3,3), dtype='uint8')/9
 
-res = convolution(img, kernel)
+res = convolution(img, kernel, p=1)
 
 
 #cvres = cv2.filter2D(img, -1, kernel)
@@ -69,6 +62,7 @@ res = convolution(img, kernel)
 if not os.path.exists('out'):
     os.mkdir('out')
 
+
 plt.imshow(res, cmap='gray')
 plt.show()
 res = Image.fromarray(res)
@@ -76,7 +70,8 @@ res.save('out/a_3_3.png')
 
 
 kernel = np.ones((5,5), dtype='uint8')/25
-res = convolution(img, kernel)
+res = convolution(img, kernel=kernel, p=2)
+
 
 plt.imshow(res, cmap='gray')
 plt.show()
