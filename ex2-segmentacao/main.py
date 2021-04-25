3e1e6e89f8ac231c14cd1f456af148b132450436
Max <max.mjm.melo@gmail.com>
2021-04-25 11:51:14 -0400

    Preparação métodos do exercício 2.

---
+++ ex2-segmentacao/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import copy
+
+def kmeans(data, k):
+    pass
+
+def imageToData(I):
+    pass
+
+def segmentImage(I, k):
+    pass
